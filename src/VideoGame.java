73e53bd530ab340332bebdc68e4f113413ee4765
David <brushdm@gmail.com>
2022-05-23 22:27:03 -0400

    Added videogame subclass

---
+++ src/VideoGame.java
@@ -0,0 +1,22 @@
+
+public class VideoGame extends Media {
+
+	@Override
+	public void checkIn() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void checkOut() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void renew() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
