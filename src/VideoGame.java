9c37818df7da92a0d13c14b36013133f3fd8e23e
David <brushdm@gmail.com>
2022-05-24 21:49:27 -0400

    Work started on VideoGame Class. More to follow

--- src/VideoGame.java
+++ src/VideoGame.java
@@ -1,4 +1,42 @@
 
-public class VideoGame {
+public class VideoGame extends Media {
+	
+	String publisher;
+	String console;
+	
+	
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+	public String getConsole() {
+		return console;
+	}
+
+	public void setConsole(String console) {
+		this.console = console;
+	}
+
+	@Override
+	public void checkIn() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void checkOut() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void renew() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
