31133fda196bf75c5351a3223131377d919c4eb6
Heather Swank <hdamoth@gmail.com>
2022-05-26 19:42:21 -0400

    fixed typos

--- src/libraryApp.java
+++ src/libraryApp.java
@@ -2,27 +2,27 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class libraryApp {
-
+	
 	public static void main(String[] args) {
-		// data
+		//data ArrayList
 		ArrayList<Media> catalog = new ArrayList<>();
-		// movies
+		//movies
 		catalog.add(new Movie(new ArrayList<String>(Arrays.asList("Steven Spielberg")), 124, "Jaws", "1975", "Thriller"));
 		catalog.add(new Movie(new ArrayList<String>(Arrays.asList("George Lucas")), 121, "Star Wars: A New Hope", "1977", "Sci-Fi"));
 		catalog.add(new Movie(new ArrayList<String>(Arrays.asList("Peter Jackson")), 169, "The Hobbit", "2012", "Fantasy"));
 		catalog.add(new Movie(new ArrayList<String>(Arrays.asList("Martin Scorsese")), 180, "The Wolf Of Wallstreet", "2013", "Drama"));
-		// books
+		//books
 		catalog.add(new Book("Fear And Loathing In Las Vegas", new ArrayList<String>(Arrays.asList("Hunter S. Thompson")), "1976", "Fiction"));
 		catalog.add(new Book("Animal Farm", new ArrayList<String>(Arrays.asList("George Orwell")), "1945", "Dystopian Fiction"));
 		catalog.add(new Book("A Game Of Thrones", new ArrayList<String>(Arrays.asList("George R. R. Martin")), "1996", "Fantasy"));
 		catalog.add(new Book("Grant", new ArrayList<String>(Arrays.asList("Ron Chernow")), "2017", "Biography"));
-		// video games
-		catalog.add(new Game("Sony", "PlayStation", "Crash Bandicoot", "1996", "Platformer"));
-		catalog.add(new Game("Microsoft", "Xbox", "Halo", "2001", "First Person Shooter"));
-		catalog.add(new Game("Nintendo", "N64", "Super Mario 64", "1996", "Platformer"));
-		catalog.add(new Game("Blizzard", "PC", "World of Warcraft", "2004", "MMO RPG"));
+		//video games
+		catalog.add(new Game("Sony", "PlayStation", "Crash Bandicoot", "1996","Platformer"));
+		catalog.add(new Game("Microsoft", "Xbox", "Halo", "2001","First Person Shooter"));
+		catalog.add(new Game("Nintendo", "N64", "Super Mario 64", "1996","Platformer"));
+		catalog.add(new Game("Blizzard", "PC", "World of Warcraft", "2004","MMO RPG"));
 	}
-
+	
 	public static void printCatalog(ArrayList<Media> catalog) {
 		System.out.printf("%-35s%-20s%n", "Title", "Format");
 		System.out.println("--------------------------------------------");
@@ -47,7 +47,7 @@ public class libraryApp {
 			}
 		}
 	}
-
+	
 	public static void searchByCreator(String creator, ArrayList<Media> catalog) {
 		System.out.println("\n\nSearch results for \"" + creator + "\":\n");
 		for (Media item : catalog) {
